80e46a338f8958251feddb293eb5ba0253477537
aqsaShahid-empglabs <aqsa.shahid@empglabs.com>
2023-02-26 09:53:50 -0800

    Initial

--- app/src/main/java/com/appalerts/MainActivity.kt
+++ app/src/main/java/com/appalerts/MainActivity.kt
@@ -2,10 +2,12 @@ package com.appalerts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.snackbars.CustomSnackBars
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CustomSnackBars.showCustomToast(this)
     }
 }
