958cc4f4302bc78e4bade82683300bf6b5d9535e
aqsaShahid-empglabs <aqsa.shahid@empglabs.com>
2023-02-27 00:20:15 -0800

    Initial

--- SnackBars/src/main/java/com/snackbars/CustomSnackBars.java
+++ SnackBars/src/main/java/com/snackbars/CustomSnackBars.java
@@ -5,6 +5,6 @@ import android.widget.Toast;
 
 public class CustomSnackBars {
     public static void showCustomToast(Context context){
-        Toast.makeText(context, "My testing amazing toast", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "My testing amazing toast ", Toast.LENGTH_LONG).show();
     }
 }
