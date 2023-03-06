5589f833da90b7c1a8d55159d42ce05e86302b35
aqsaShahid-empglabs <aqsa.shahid@empglabs.com>
2023-03-05 22:50:38 -0800

    Bug fix

--- SnackBars/src/main/java/com/snackbars/CustomSnackBars.java
+++ SnackBars/src/main/java/com/snackbars/CustomSnackBars.java
@@ -5,6 +5,7 @@ import android.widget.Toast;
 
 public class CustomSnackBars {
     public static void showCustomToast(Context context){
+        String t;
         Toast.makeText(context, "My testing amazing toast ", Toast.LENGTH_LONG).show();
     }
 }
